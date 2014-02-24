376ef7722f0c09caff265a464afba15c103038b2
Andrew Dixon <andrew.dixon@twitch.tv>
2014-02-24 09:57:07 -0800

    who knows

--- lib/cashregister.rb
+++ lib/cashregister.rb
@@ -1,2 +1,69 @@
-class Cashregister
+class CashRegister
+  def initialize(coins=[25,10,5,1])
+    if coins.class != Array || coins.map do |coin|
+      coin.class.ancestors.include?(Integer)
+    end.include?(false)
+      raise "CashRegister must be initialized with an Array of Integers"
+    end
+
+    @coins = coins
+
+    @optimal_change = Hash.new do |hash, key|
+      if key < coins.min
+        hash[key] = Change.new(coins)
+      elsif coins.include?(key)
+        hash[key] = Change.new(coins).add(key)
+      else
+        possible_coins = Array.new(coins)
+       # possible_coins.reject! { |coin| coin > key }
+
+        #          possible_coins.reject! do |small_coin|
+        #            coins.map do |large_coin|
+        #              large_coin % small_coin == 0
+        #            end.include?(true)
+        #          end
+
+        possible_change = possible_coins.sort.reverse.map do |coin|
+          hash[key - coin].add(coin)
+        end
+        puts "key: #{key}"
+puts "possible change: #{possible_change}"
+        possible_change.reject! { |change| change.value != key }
+puts "possible change filtered: #{possible_change}"
+
+        puts "final answer: #{possible_change.min { |a, b| a.size <=> b.size }}"
+        hash[key] = possible_change.min { |a, b| a.size <=> b.size } || Change.new(coins)
+      end
+    end
+  end
+
+  def make_change(amount)
+    if !amount.class.ancestors.include?(Integer)
+      raise "make_change() expects an Integer argument but received an argument of type #{amount.class}"
+    end
+
+    return(@optimal_change[amount])
+  end
+end
+
+class Change < Hash
+  def initialize(coins)
+    coins.sort.reverse.map { |coin| self.merge!({coin.to_s => 0}) }
+  end
+
+  def add(coin)
+    self.merge({coin.to_s => self[coin] + 1})
+  end
+
+  def value
+    value_of_change = 0
+    self.each do |key, value|
+      value_of_change += key.to_i * value
+    end
+    return(value_of_change)
+  end
+
+  def size
+    self.values.inject(:+)
+  end
 end
--- spec/cashregister_spec.rb
+++ spec/cashregister_spec.rb
@@ -1,12 +1,103 @@
 require 'spec_helper'
 require 'cashregister'
 
-describe Cashregister do
+describe CashRegister do
   describe "new" do
     it "should instantiate" do
       lambda {
-        Cashregister.new
+        CashRegister.new
       }.should_not raise_exception
+    end
+    it "takes an array of integers" do
+      expect { CashRegister.new([10,7,1]) }.to_not raise_exception
+    end
+    it "throws an error when given bad input" do
+      expect { CashRegister.new(5) }.to raise_exception
+      expect { CashRegister.new(["a", "b", "c"]) }.to raise_exception
+    end
+  end
+
+  describe ".make_change" do
+    subject(:make_change) { CashRegister.new(coins).make_change(amount) }
+    let(:coins)  { [25,10,5,1] }
+    let(:amount) { 123 }
+
+    it { should eq({'25' => 4, '10' => 2, '5' => 0, '1' => 3}) }
+
+    context 'with bad input' do
+      let(:amount) { 'b' }
+
+      specify { expect { make_change }.to raise_error }
+    end
+
+    context 'another example' do
+      let(:amount) { 47 }
+      it { should eq({'25' => 1, '10' => 2, '5' => 0, '1' => 2}) }
+    end
+
+    it 'should handle the base case' do
+      CashRegister.new.make_change(25).should eq({'25' => 1, '10' => 0, '5' => 0, '1' => 0})
+    end
+
+    it 'should handle a simple case' do
+      CashRegister.new.make_change(50).should eq({'25' => 2, '10' => 0, '5' => 0, '1' => 0})
+    end
+
+    context 'crazy foreign coins' do
+      let(:coins)  { [10,7,1] }
+      let(:amount) { 14 }
+
+      it { should eq({'10' => 0, '7' => 2, '1' => 0}) }
+
+      it 'works for the null case' do
+        CashRegister.new([10,7,1]).make_change(0).should eq({'10' => 0, '7' => 0, '1' => 0})
+      end
+
+      it 'works for the base case' do
+        CashRegister.new([10,7,1]).make_change(1).should eq({'10' => 0, '7' => 0, '1' => 1})
+      end
+    end
+  end
+end
+
+describe Change do
+  describe "#new" do
+    subject(:change) { Change.new([25,10,5,1]) }
+
+    specify { expect { change }.to_not raise_error }
+    it { should eq({'25' => 0, '10' => 0, '5' => 0, '1' => 0}) }
+  end
+
+  describe ".add!" do
+    let(:change) { Change.new([25,10,5,1]) }
+    let(:coin) { 5 }
+
+    it 'adds a coin to the pile of change' do
+      change.add!(coin)
+      change.should eq({'25' => 0, '10' => 0, '5' => 1, '1' => 0})
+    end
+  end
+
+  describe ".value" do
+    let(:change) { Change.new([25,10,5,1]) }
+
+    it 'returns the value of the pile of change' do
+      change.add!(25)
+      change.add!(5)
+      change.add!(1)
+      change.value.should eq(31)
+    end
+  end
+
+  describe ".size" do
+    subject(:change) { Change.new([25,10,5,1]) }
+
+    its(:size) { should eq(0) }
+    it 'returns the right value' do
+      change.add!(25)
+      change.add!(25)
+      change.add!(1)
+      change.size.should eq(3)
     end
   end
 end
